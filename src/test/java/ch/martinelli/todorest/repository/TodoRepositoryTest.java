d7e21a676e03827567430bd9b4e485fa826a0f69
Simon Martinelli <simon@martinelli.ch>
2024-10-11 11:23:40 +0200

    Added saveAndFlush and disabled the SQL logging of DataJpaTest

--- src/test/java/ch/martinelli/todorest/repository/TodoRepositoryTest.java
+++ src/test/java/ch/martinelli/todorest/repository/TodoRepositoryTest.java
@@ -1,23 +1,32 @@
 package ch.martinelli.todorest.repository;
 
 import ch.martinelli.todorest.web.Todo;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
-@DataJpaTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = false)
 class TodoRepositoryTest {
 
     @Autowired
     private TodoRepository todoRepository;
+    
+    @BeforeEach
+    void setUp() {
+        Todo test = new Todo(1L, "Test");
+        test.setCompleted(true);
+        // saveAndFlush damit das Insert Statement produziert wirdz
+        todoRepository.saveAndFlush(test);
+    }
 
     @Test
     void findAllByCompleted() {
         List<Todo> allByCompleted = todoRepository.findAllByCompleted(true);
-
-        Assertions.assertThat(allByCompleted).isEmpty();
+        assertThat(allByCompleted).size().isEqualTo(1);
     }
 }
